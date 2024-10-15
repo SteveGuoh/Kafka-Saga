7ea2a8facbe4e0e818cbde1cccca3077229ad544
SteveGuoh <hghandle@outlook.com>
2024-10-15 22:21:15 +1100

    SAGA INIT

---
+++ products-service/src/main/java/com/appsdeveloperblog/products/service/handler/ProductCommandsHandler.java
@@ -0,0 +1,36 @@
+package com.appsdeveloperblog.products.service.handler;
+
+import com.appsdeveloperblog.core.dto.Product;
+import com.appsdeveloperblog.core.dto.commands.ReserveProductCommand;
+import com.appsdeveloperblog.products.service.ProductService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.kafka.annotation.KafkaHandler;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.stereotype.Component;
+
+@Component
+@KafkaListener(topics = "${products.commands.topic.name}")
+public class ProductCommandsHandler {
+
+    public ProductCommandsHandler(ProductService productService) {
+        this.productService = productService;
+    }
+
+    private final ProductService productService;
+    private final Logger logger = LoggerFactory.getLogger(ProductCommandsHandler.class.getName());
+
+    @KafkaHandler
+    public void handleCommand(@Payload ReserveProductCommand command) {
+
+        try {
+            Product desiredProduct = new Product(command.getProductId(), command.getProductQuantity());
+            Product reservedProduct = productService.reserve(desiredProduct, command.getOrderId());
+        } catch (Exception e) {
+            logger.error(e.getLocalizedMessage(), e);
+        }
+
+    }
+
+}
