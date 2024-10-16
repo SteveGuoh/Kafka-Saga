551e01626068c0da3f321e8a8d1d5800001d039c
SteveGuoh <hghandle@outlook.com>
2024-10-17 00:08:01 +1100

    done with saga happy path

--- orders-service/src/main/java/com/appsdeveloperblog/orders/config/KafkaConfig.java
+++ orders-service/src/main/java/com/appsdeveloperblog/orders/config/KafkaConfig.java
@@ -17,6 +17,12 @@ public class KafkaConfig {
     @Value("${products.commands.topic.name}")
     private String productsCommandsTopicName;
 
+    @Value("${payments.commands.topic.name}")
+    private String paymentsCommandsTopicName;
+
+    @Value("${orders.commands.topic.name}")
+    private String ordersCommandsTopicName;
+
     private final static Integer TOPIC_REPLICATION_FACTOR = 3;
     private final static Integer TOPIC_PARTITIONS = 3;
 
@@ -36,6 +42,22 @@ public class KafkaConfig {
     @Bean
     NewTopic createProductsCommandsTopic() {
         return TopicBuilder.name(productsCommandsTopicName)
+                .partitions(TOPIC_PARTITIONS)
+                .replicas(TOPIC_REPLICATION_FACTOR)
+                .build();
+    }
+
+    @Bean
+    NewTopic createPaymentsCommandsTopic() {
+        return TopicBuilder.name(paymentsCommandsTopicName)
+                .partitions(TOPIC_PARTITIONS)
+                .replicas(TOPIC_REPLICATION_FACTOR)
+                .build();
+    }
+
+    @Bean
+    NewTopic createOrdersCommandsTopic() {
+        return TopicBuilder.name(ordersCommandsTopicName)
                 .partitions(TOPIC_PARTITIONS)
                 .replicas(TOPIC_REPLICATION_FACTOR)
                 .build();
--- products-service/src/main/java/com/appsdeveloperblog/products/service/handler/ProductCommandsHandler.java
+++ products-service/src/main/java/com/appsdeveloperblog/products/service/handler/ProductCommandsHandler.java
@@ -2,11 +2,15 @@ package com.appsdeveloperblog.products.service.handler;
 
 import com.appsdeveloperblog.core.dto.Product;
 import com.appsdeveloperblog.core.dto.commands.ReserveProductCommand;
+import com.appsdeveloperblog.core.dto.events.ProductReservationFailedEvent;
+import com.appsdeveloperblog.core.dto.events.ProductReservedEvent;
 import com.appsdeveloperblog.products.service.ProductService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.annotation.KafkaHandler;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
 
@@ -14,12 +18,17 @@ import org.springframework.stereotype.Component;
 @KafkaListener(topics = "${products.commands.topic.name}")
 public class ProductCommandsHandler {
 
-    public ProductCommandsHandler(ProductService productService) {
+    public ProductCommandsHandler(ProductService productService, KafkaTemplate<String, Object> kafkaTemplate,
+                                  @Value("${products.events.topic.name}") String productEventsTopicName) {
         this.productService = productService;
+        this.kafkaTemplate = kafkaTemplate;
+        this.productEventsTopicName = productEventsTopicName;
     }
 
     private final ProductService productService;
     private final Logger logger = LoggerFactory.getLogger(ProductCommandsHandler.class.getName());
+    private final KafkaTemplate<String, Object> kafkaTemplate;
+    private final String productEventsTopicName;
 
     @KafkaHandler
     public void handleCommand(@Payload ReserveProductCommand command) {
@@ -27,8 +36,14 @@ public class ProductCommandsHandler {
         try {
             Product desiredProduct = new Product(command.getProductId(), command.getProductQuantity());
             Product reservedProduct = productService.reserve(desiredProduct, command.getOrderId());
+            ProductReservedEvent productReservedEvent = new ProductReservedEvent(command.getOrderId(), reservedProduct.getId(),
+                    reservedProduct.getPrice(), reservedProduct.getQuantity());
+            kafkaTemplate.send(productEventsTopicName, productReservedEvent);
         } catch (Exception e) {
             logger.error(e.getLocalizedMessage(), e);
+            ProductReservationFailedEvent productReservationFailedEvent = new ProductReservationFailedEvent(command.getProductId(),
+                    command.getOrderId(), command.getProductQuantity());
+            kafkaTemplate.send(productEventsTopicName, productReservationFailedEvent);
         }
 
     }
